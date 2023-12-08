cc8547140738d9bc53c221ac9eefebbda8fccddf
DaichiIkezawa <ikesawa.daichi@gmail.com>
2023-12-08 13:59:30 -0600

    Actualizacion 6
    Actualizacion de gran parte del proyecto tienda

---
+++ Tienda_v2/src/main/java/com/tienda_v2/domain/Producto.java
@@ -0,0 +1,41 @@
+package com.tienda_v2.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="producto")
+public class Producto implements Serializable {
+    
+    private static final long serialVersionUID = 1l;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_producto")
+    private Long idProducto;
+    private Long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name="id_categoria")
+    Categoria categoria;
+    
+    public Producto() {
+    }
+
+    public Producto(String descripcion, String detalle, double precio, int existencias, String imagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = imagen;
+        this.activo = activo;
+    }
+}
---
+++ Tienda_v2/src/main/java/com/tienda_v2/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.tienda_v2.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-17f13.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-17f13-firebase-adminsdk-3yayd-781b639bad.json";
+}
